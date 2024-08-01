630cab7db7d98f298946c97d25604ea0d45843fe
Maverick Wong <mvrck.wong@gmail.com>
2024-08-02 02:43:55 +0800

    update

---
+++ models/stg/stg_fct_returns.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='table'
+) }}
+
+-- Get the return data from the raw schema
+WITH source AS (
+    SELECT 
+        return_date::DATE
+        ,product_id::INT
+        ,store_id::INT
+        ,quantity::INT
+    FROM 
+        raw.returns
+)
+
+SELECT 
+    *
+FROM 
+    source
+ORDER BY 
+    return_date ASC;
